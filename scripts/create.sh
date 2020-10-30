a247bbaacd8c573005377683612fa32046fdd5b8
Martin Brenn <brenn@depon.net>
2020-10-30 19:31:40 +0100

    docify

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,4 +1,4 @@
-asciidoctor -D ../output ../docs/index.adoc
+asciidoctor -a toc -a docinfo=shared -D ../output ../docs/index.adoc
 mkdir -p ../output/images
 cp -r ../docs/images/* ../output/images/
 
