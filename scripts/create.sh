f3edc2530f58bd74ef3352ac4ad590b9a5db5a88
Martin Brenn <brenn@depon.net>
2020-10-30 19:32:23 +0100

    Fix for docify

--- scripts/create.sh
+++ scripts/create.sh
@@ -1,4 +1,4 @@
-asciidoctor -a toc -a docinfo=shared -D ../output ../docs/index.adoc
+asciidoctor -a docinfo=shared -D ../output ../docs/index.adoc
 mkdir -p ../output/images
 cp -r ../docs/images/* ../output/images/
 
