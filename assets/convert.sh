425ce67b809fcac29b001f8a1dfca60a3816d084
Martin Brenn <brenn@depon.net>
2021-02-21 20:13:50 +0100

    Several updates

--- assets/convert.sh
+++ assets/convert.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 umlet -action=convert -format=png -filename=reports_classes.uxf
+umlet -action=convert -format=png -filename=meetingmeister.usecases.uxf
+umlet -action=convert -format=png -filename=meetingmeister.actions.uxf
 
 mv reports_classes.uxf.png ../docs/images/
 
