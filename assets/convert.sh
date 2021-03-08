bb3ba250ebdeaf214139959e7fa3086fc996e005
Martin Brenn <brenn@depon.net>
2021-03-08 22:23:11 +0100

    Update

--- assets/convert.sh
+++ assets/convert.sh
@@ -3,4 +3,5 @@ umlet -action=convert -format=png -filename=reports_classes.uxf
 umlet -action=convert -format=png -filename=meetingmeister.usecases.uxf
 umlet -action=convert -format=png -filename=meetingmeister.actions.uxf
 umlet -action=convert -format=png -filename=datenmeister.web.navigation.uxf
+umlet -action=convert -format=png -filename=datenmeister.web.ui.uxf
 mv *.uxf.png ../docs/images/
