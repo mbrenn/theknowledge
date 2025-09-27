4761a9f2c2d2a5e4dc7800206a3f3a7854c1a1f4
Martin Brenn <brenn@depon.net>
2025-09-27 15:12:05 +0200

    Script is running

--- scripts/create.py
+++ scripts/create.py
@@ -1,6 +1,7 @@
 
 import os
 import subprocess
+import shutil
 
 root_dir = '../docs'
 base_output_dir = '../output'
@@ -9,22 +10,34 @@ base_output_dir = '../output'
 for dirpath, dirnames, filenames in os.walk(root_dir):
     # Filter .adoc files in the current directory
     adoc_files = [os.path.join(dirpath, f) for f in filenames if f.endswith('.adoc')]
+    png_files = [os.path.join(dirpath, f) for f in filenames if f.endswith('.png')]
 
     print(dirpath)
+
+    # Compute relative path from root_dir
+    rel_path = os.path.relpath(dirpath, root_dir)
+    output_dir = os.path.join(base_output_dir, rel_path)
+
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
     
     # If there are any .adoc files, run the asciidoctor command
     if adoc_files:
 
-        # Compute relative path from root_dir
-        rel_path = os.path.relpath(dirpath, root_dir)
-        output_dir = os.path.join(base_output_dir, rel_path)
-
-        # Create the output directory if it doesn't exist
-        os.makedirs(output_dir, exist_ok=True)
-
         # Run asciidoctor with the specific output directory
         command = ['asciidoctor', '-a', 'docinfo=shared', '-D', output_dir] + adoc_files
         subprocess.run(command)
+
+    if png_files:
+        # Copy all found png files to target directry
+        
+        for png_file in png_files:
+            filename = os.path.join(output_dir, os.path.basename(png_file))
+            if os.path.exists(filename):
+                os.remove(filename)
+            shutil.copyfile(png_file, filename)
+
+        
 
 
 print("Asciidoctor processing completed for all subdirectories.")
